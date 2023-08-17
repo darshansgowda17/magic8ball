56e1a9ba87e18a232d0a5283248e0823e35c6e50
darshansgowda17 <darshansgowda111@gmail.com>
2023-08-17 20:23:45 +0530

    dynamic functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,70 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int answer = 1;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
         appBar: AppBar(
-          title: Center(child: Text("Magic 8 Ball"),
+          title: Center(
+            child: Text("Magic 8 Ball"),
           ),
           backgroundColor: Colors.blue[900],
-          ),
+        ),
         body: Padding(
           padding: EdgeInsets.all(25.0),
-          child: Center(child: Expanded(child: Image.asset("images/ball1.png"))),
-        ),
+          child: Center(child: InkWell(
+              onTap: () {
+                setState(() {
+                  answer = Random().nextInt(5)+1;
+                });
+                },
+            child: Ink.image(
+              width: 300,
+              height: 300,
+              image: AssetImage("images/ball$answer.png"),
+            )),)),
         ),
 
     );
   }
 }
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.blue,
+//         appBar: AppBar(
+//           title: Center(child: Text("Magic 8 Ball"),
+//           ),
+//           backgroundColor: Colors.blue[900],
+//           ),
+//         body: Padding(
+//           padding: EdgeInsets.all(25.0),
+//           child: Center(child: Expanded(child: Image.asset("images/ball1.png"))),
+//         ),
+//         ),
+//
+//     );
+//   }
+// }
 
